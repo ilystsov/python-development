e29efa0455634065f02421a87257077044b63696
ilya <lystsov.ilya@gmail.com>
2024-03-07 22:50:49 +0300

    modify addmon command

--- 20240304/1/prog.py
+++ 20240304/1/prog.py
@@ -1,6 +1,6 @@
-import shlex
-
 import cowsay
+
+import shlex
 
 
 class Player:
@@ -10,9 +10,11 @@ class Player:
 
 
 class Monster:
-    def __init__(self, name: str, greetings_message: str) -> None:
+    def __init__(self, name: str, greetings_message: str, hitpoints: int) -> None:
         self.greetings_message: str = greetings_message
         self.name = name
+        self.hitpoints = hitpoints
+
 
 class MultiUserDungeon:
     def __init__(self, field_size: int) -> None:
@@ -22,19 +24,19 @@ class MultiUserDungeon:
 
     def move_player(self, direction: str) -> None:
         delta_x, delta_y = 0, 0
-        if direction == 'up':
+        if direction == "up":
             delta_y = -1
-        elif direction == 'down':
+        elif direction == "down":
             delta_y = 1
-        elif direction == 'left':
+        elif direction == "left":
             delta_x = -1
-        elif direction == 'right':
+        elif direction == "right":
             delta_x = 1
 
         self.player.x = (self.player.x + delta_x) % self.field_size
         self.player.y = (self.player.y + delta_y) % self.field_size
 
-        print(f'Moved to ({self.player.x}, {self.player.y})')
+        print(f"Moved to ({self.player.x}, {self.player.y})")
 
         if (self.player.x, self.player.y) in self.monsters:
             self.encounter(self.player.x, self.player.y)
@@ -45,7 +47,9 @@ class MultiUserDungeon:
         name = monster.name
         print(cowsay.cowsay(text, cow=name))
 
-    def add_monster(self, name: str, x: int, y: int, greetings_message: str) -> None:
+    def add_monster(
+        self, name: str, hitpoints: int, x: int, y: int, greetings_message: str
+    ) -> None:
         if name not in cowsay.list_cows():
             print("Cannot add unknown monster")
             return
@@ -53,7 +57,40 @@ class MultiUserDungeon:
         print(f"Added monster {name} to ({x}, {y}) saying {greetings_message}")
         if (x, y) in self.monsters:
             print("Replaced the old monster")
-        self.monsters[(x, y)] = Monster(name, greetings_message)
+        self.monsters[(x, y)] = Monster(name, greetings_message, hitpoints)
+
+    def parse_addmon(self, params: list) -> tuple:
+        expected_params_length = 8
+        if len(params) != expected_params_length:
+            raise ValueError
+        name = params[0]
+        param_pos = 1
+        hitpoints, x, y, greetings_message = [None] * 4
+        while param_pos < expected_params_length:
+            if params[param_pos] == "hello":
+                if greetings_message is not None:
+                    raise ValueError
+                greetings_message = params[param_pos + 1]
+                param_pos += 2
+            elif params[param_pos] == "hp":
+                if (hitpoints is not None) or (not params[param_pos + 1].isdigit()):
+                    raise ValueError
+                hitpoints = int(params[param_pos + 1])
+                if hitpoints <= 0:
+                    raise ValueError
+                param_pos += 2
+            elif params[param_pos] == "coords":
+                if (
+                    (x is not None) or (y is not None)
+                    or (not params[param_pos + 1].isdigit())
+                    or (not params[param_pos + 2].isdigit())
+                ):
+                    raise ValueError
+                x, y = int(params[param_pos + 1]), int(params[param_pos + 2])
+                param_pos += 3
+            else:
+                raise ValueError
+        return name, hitpoints, x, y, greetings_message
 
     def play(self) -> None:
         while True:
@@ -63,16 +100,19 @@ class MultiUserDungeon:
                 break
 
             match command:
-                case [('up' | 'down' | 'left' | 'right') as direction]:
+                case [("up" | "down" | "left" | "right") as direction]:
                     self.move_player(direction)
-                case ['addmon', *params]:
-                    if len(params) == 4 and params[1].isdigit() and params[2].isdigit():
-                        self.add_monster(params[0], int(params[1]), int(params[2]), params[3])
-                    else:
-                        print('Invalid arguments')
+                case ["addmon", *params]:
+                    try:
+                        name, hitpoints, x, y, greetings_message = self.parse_addmon(
+                            params
+                        )
+                        self.add_monster(name, hitpoints, x, y, greetings_message)
+                    except ValueError:
+                        print("Invalid arguments")
                 case _:
                     print("Invalid command")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MultiUserDungeon(10).play()
