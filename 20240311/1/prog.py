c328ff1cee5ccd6820c368c3018a093c2342b5f1
ilya <lystsov.ilya@gmail.com>
2024-03-17 19:34:02 +0300

    add different types of weapons

--- 20240311/1/prog.py
+++ 20240311/1/prog.py
@@ -42,6 +42,7 @@ class MultiUserDungeon:
         self.field_size: int = field_size
         self.player: Player = Player()
         self.monsters: dict[tuple[int, int], Monster] = {}
+        self.weapons: dict[str, int] = {'sword': 10, 'spear': 15, 'axe': 20}
         self.custom_monsters: dict[str, Monster] = {"jgsbat": jgsbat}
 
     def move_player(self, direction: str) -> None:
@@ -72,15 +73,14 @@ class MultiUserDungeon:
         else:
             print(cowsay.cowsay(text, cow=name))
 
-    def attack(self) -> None:
+    def attack(self, weapon: str) -> None:
         player_coordinates = (self.player.x, self.player.y)
         if player_coordinates not in self.monsters:
             print("No monster here")
             return
 
-        base_damage = 10
         monster = self.monsters[player_coordinates]
-        damage = min(monster.hitpoints, base_damage)
+        damage = min(monster.hitpoints, self.weapons[weapon])
         print(f"Attacked {monster.name},  damage {damage} hp")
         monster.hitpoints -= damage
         if monster.hitpoints == 0:
@@ -135,6 +135,17 @@ class MultiUserDungeon:
                 raise ValueError
         return name, hitpoints, x, y, greetings_message
 
+    def parse_attack(self, params: list) -> str:
+        base_weapon = 'sword'
+        match params:
+            case ['with', weapon]:
+                if weapon not in self.weapons:
+                    raise ValueError("Unknown weapon")
+                return weapon
+            case []:
+                return base_weapon
+            case _:
+                raise ValueError("Invalid arguments")
 
 class MultiUserDungeonShell(cmd.Cmd):
     intro = "<<< Welcome to Python-MUD 0.1 >>>"
@@ -165,7 +176,12 @@ class MultiUserDungeonShell(cmd.Cmd):
             print("Invalid arguments")
 
     def do_attack(self, arg: str) -> None:
-        self.game.attack()
+        params = shlex.split(arg)
+        try:
+            weapon = self.game.parse_attack(params)
+            self.game.attack(weapon)
+        except ValueError as error:
+            print(error)
 
     def do_EOF(self, arg: str) -> bool:
         return True
